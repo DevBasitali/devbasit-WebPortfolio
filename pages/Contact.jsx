7b8abbdd0756316aad61762754779f18d9befad9
DevBasitali <rajaalix2022@gmail.com>
2024-10-14 20:49:51 +0500

    updating contact us form

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -39,70 +39,72 @@ try {
 }
   return (
     <>
-      <section className="cta_section relative w-full h-screen" id="contactUs">
+      <section className=" cta_section relative w-full h-screen" id="contactUs">
       <Image
-        src='/backgrounds/contactBg.svg'
+        src='/backgrounds/heroBg.svg'
         alt="Call to Action Background"
         layout="fill"
         objectFit="cover"
         quality={75}
-        className="-z-10"
+        className="-z-10 opacity-99"
       />
-      
-        <div className="cardmorphism w-[30%]">
-          <form 
-          onSubmit={handleSubmit}
-          >
-            <label
-              htmlFor="fullname"
-              className="block text-sm font-medium text-black"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              id="fullname"
-              
-              onChange={(e) => setName(e.target.value)}
-              className="input_text"
-            />
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-black mt-5"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              
-              onChange={(e) => setEmail(e.target.value)}
-              className="input_text"
-            />
-            <label
-              htmlFor="message"
-              className="block text-sm font-medium text-black mt-5"
-            >
-              Message
-            </label>
+        <div className="cardmorphism w-[80%] lg:w-[30%] ">
+  <h1 className="text-4xl font-bold text-teal-600 flex justify-center my-4">
+    Contact Us
+  </h1>
+  <form 
+  onSubmit={handleSubmit}
+  >
+    <label
+      htmlFor="fullname"
+      className="block text-sm font-medium text-white"
+    >
+      Name
+    </label>
+    <input
+      type="text"
+      id="fullname"
+      placeholder="Enter Full name"
+      onChange={(e) => setName(e.target.value)}
+      className="input_text placeholder:text-black"
+    />
+    <label
+      htmlFor="email"
+      className="block text-sm font-medium text-white mt-5"
+    >
+      Email
+    </label>
+    <input
+      type="email"
+      id="email"
+      placeholder="Enter Email"
+      onChange={(e) => setEmail(e.target.value)}
+      className="input_text placeholder:text-black"
+    />
+    <label
+      htmlFor="message"
+      className="block text-sm font-medium text-white mt-5"
+    >
+      Message
+    </label>
 
-            <textarea
-              id="message"
-              placeholder="Enter your message here"
-              onChange={(e) => setMsg(e.target.value)}
-              className="input_text w-full"
-              rows={4}
-            />
-            {alert && <Alert title="Sucess" desc={alert}/>}
+    <textarea
+      id="message"
+      placeholder="Enter your message here"
+      onChange={(e) => setMsg(e.target.value)}
+      className="input_text w-full placeholder:text-black"
+      rows={4}
+    />
+    {alert && <Alert title="Sucess" desc={alert}/>}
 
-            <button
+    <button
               
-              className="btn_primary active:text-opacity-75 sm:w-auto text-md" 
-            >
-              Send Message
-            </button>
-          </form>
-        </div>
+      className="btn_primary active:text-opacity-75 sm:w-auto text-md" 
+    >
+      Send Message
+    </button>
+  </form>
+</div>
       </section>
     </>
   );
